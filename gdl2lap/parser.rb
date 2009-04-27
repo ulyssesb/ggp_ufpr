02ea83f6f3a8b9b645f04c65bed10ab05960da7a
Ulysses Bonfim <ulyssesb@gmail.com>
2009-04-27 07:04:20 -0300

    Inserção de relações na pilha

--- gdl2lap/parser.rb
+++ gdl2lap/parser.rb
@@ -7,34 +7,70 @@ end
 
 
 File.open("#{ARGV}") do |handle|
-	
+
 	# Pilha de relacoes
 	rel_stack = []
 
 	# Hash das relacoes
-	relations_hash = []
+	relations_hash = Hash.new
 	
-	# Controle dos '(' ')'
-	p_control = 0
-
 	handle.each_line do |linha|
-		# Pega a relation e guarda na hash
-		relations_hash = linha.gsub(/\(.*\)/, '')
-		
-		# Apaga espacos em branco
-		linha.delete " "
 
-		# Adiciona os elementos da linha na pilha
-		rel_stack += [linha.scan(/\w+|\(|\)|\<\=|\~\w+|\&|\^/)]
+		# Separa a string da linha para iteracao
+		l_elems = linha.scan(/\w+|\(|\)|\<\=|\~\w+|\&|\^|\,|\?w+/)
 
-		rel.stack.each do |elem|
+		print "["
+		rel_stack.each { |e| print "'#{e}' "}
+		print "]\n"
+
+
+		l_elems.each do |elem|
 			case elem
-				when "("
-				p_control += 1
-				when ")"
-				p_control -= 1
-				when 
+
+			# do nothing
+			when "("
+		
+			# rel) ... retira a relacao da pilha
+			when  ")" 
+				rel_stack.pop
 				
+			# Variaveis (maiusculas ou comecando com '?')
+			when /[A-Z]/, /\?/
+				# pilha vazia, nada pra fazer
+				if rel_stack.empty?
+					next 
+				end
+
+				# Aumenta o numero de argumentos
+				rel_stack.last[1] += 1
+
+			# Valores (entradas instanciadas)
+			when /\d/
+				# pilha vazia, nada a fazer
+				if rel_stack.empty? 
+					next
+				end
+
+				# Inc numero de argumentos
+				rel_stack.last[1] += 1
+
+				# Extrai a relacao do topo e adiciona o valor na lista 
+				# argumentos possiveis
+				relation = rel_stack.last[0]
+				arg_num = rel_stack.last[1]
+				#relations_hash[relation][arg_num] << elem
+
+			when /\w+/, /\~\w+/
+				print "\n$#{elem}$"
+
+				# Empilha relacao + numeros de argumentos
+				rel_stack << [elem , 0]
+				puts rel_stack
+				
+				# Caso nao exista, adiciona relacao na hash
+				if relations_hash.has_key?(elem.to_sym) 
+					relations_hash[elem.to_sym] = []
+				end
 			end
 		end
 	end
