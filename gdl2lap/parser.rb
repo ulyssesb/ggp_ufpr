35b904ce1c63adb45926a56e131efad541b92501
Ulysses Bonfim <ulyssesb@gmail.com>
2009-04-28 22:12:51 -0300

    relations_hash contem as relacoes e seus argumentos

--- gdl2lap/parser.rb
+++ gdl2lap/parser.rb
@@ -13,22 +13,30 @@ File.open("#{ARGV}") do |handle|
 
 	# Hash das relacoes
 	relations_hash = Hash.new
+
+	# Flag para empilhar a proxima relacao lida
+	empilha = false
 	
+
+
 	handle.each_line do |linha|
+
+		# Ignora comentarios e linhas vazias
+		if linha[0].chr == ";" or linha == "\n"
+			next
+		end
+		
+		print "$"+linha
 
 		# Separa a string da linha para iteracao
 		l_elems = linha.scan(/\w+|\(|\)|\<\=|\~\w+|\&|\^|\,|\?w+/)
 
-		print "["
-		rel_stack.each { |e| print "'#{e}' "}
-		print "]\n"
-
-
 		l_elems.each do |elem|
 			case elem
 
-			# do nothing
+			# Ativa flag para empilhar a proxima relacao
 			when "("
+				empilha = true
 		
 			# rel) ... retira a relacao da pilha
 			when  ")" 
@@ -44,34 +52,65 @@ File.open("#{ARGV}") do |handle|
 				# Aumenta o numero de argumentos
 				rel_stack.last[1] += 1
 
-			# Valores (entradas instanciadas)
-			when /\d/
-				# pilha vazia, nada a fazer
-				if rel_stack.empty? 
-					next
+			when /\w+/, /\~\w+/
+				
+				# Relacao encontrada. Empilha e zera o numero de argumentos
+				if empilha
+					empilha = false
+					rel_stack << [elem.to_sym , 0]
+				# Relacao sem argumentos. Incremente o numero de args da relacao pai
+				else
+					rel_stack.last[1] += 1
 				end
 
-				# Inc numero de argumentos
-				rel_stack.last[1] += 1
-
-				# Extrai a relacao do topo e adiciona o valor na lista 
-				# argumentos possiveis
-				relation = rel_stack.last[0]
-				arg_num = rel_stack.last[1]
-				#relations_hash[relation][arg_num] << elem
-
-			when /\w+/, /\~\w+/
-				print "\n$#{elem}$"
-
-				# Empilha relacao + numeros de argumentos
-				rel_stack << [elem , 0]
-				puts rel_stack
-				
 				# Caso nao exista, adiciona relacao na hash
-				if relations_hash.has_key?(elem.to_sym) 
+				if not relations_hash.has_key?(elem.to_sym) 
 					relations_hash[elem.to_sym] = []
 				end
+
+				# Adiciona argumento na lista da relacao que esta no
+				# topo da pilha
+				if relations_hash[rel_stack.last[0]][rel_stack.last[1]] == nil
+					relations_hash[rel_stack.last[0]][rel_stack.last[1]] = []
+				end
+
+				if not rel_stack.last[0] == elem.to_sym 						
+					if not
+					relations_hash[rel_stack.last[0]][rel_stack.last[1]].include?(elem.to_sym) 
+						relations_hash[rel_stack.last[0]][rel_stack.last[1]] <<
+							elem.to_sym 
+					end
+				end
+					
+				
 			end
+
+			print "= '"+elem+"' -> ["
+			rel_stack.each { |e| print "'#{e}' "}
+			print "]\n"
+			
+			print ">> "
+			relations_hash.each do |e| 
+				e.each do |l|
+					if l.class == :teste.class
+						print "#{l} -> "
+					else
+						l.each do |x|
+							if not x == nil
+								print "["
+								x.each {|y| print "#{y} "}
+								print "]"
+							end
+						end
+					end
+				end
+				print "\n"
+			end
+				
+			print "<<\n\n"
+
+			
+			
 		end
 	end
 end
